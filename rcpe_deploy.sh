ece0ea01ff55390268c37cf0dde72f8e025cf50c
Jason Cannavale <jason@cannavale.com>
2012-01-09 13:18:25 -0600

    cleaning up debugging stuffs, adding logic for checking crowbar node registration.

--- rcpe_deploy.sh
+++ rcpe_deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-set -e
-set -x
+# set -e
+# set -x
 
 # remove me when done tsting
 # PXE_IMAGE_URL=file:///opt/rcb/pxeappliance-dist.qcow2
@@ -20,6 +20,7 @@ sed -i 's/^#net.ipv4.ip_forward/net.ipv4.ip_forward/' /etc/sysctl.conf
 sysctl -p /etc/sysctl.conf
 
 # Check for/Generate ssh key
+echo "Generating ssh keys.."
 if [ ! -f /home/${SUDO_USER}/.ssh/id_rsa.pub ]; then
     ssh-keygen -t rsa -f /home/${SUDO_USER}/.ssh/id_rsa -N '' -q
 fi
@@ -34,18 +35,19 @@ fi
 
 # Parse json file, extract bastion/pxeapp/infra/crowbar addresses, export to .deployrc
 echo "Extracting json values to environment variables.."
-for i in BASTION PXEAPP INFRA INFRA_MAC INFRA_DRAC CROWBAR NETMASK GATEWAY NAMESERVER CBFQDN; do  python -c "import json; import os; data = open('env.json');json_data = json.load(data); data.close(); print json_data['attributes']['network']['reserved']['$i'.lower()]" | echo "export $i=`awk '{print $0}'`" >> .deployrc; done
+for i in BASTION PXEAPP INFRA INFRA_MAC INFRA_DRAC CROWBAR NETMASK GATEWAY NAMESERVER CBFQDN NODECOUNT; do  python -c "import json; import os; data = open('env.json');json_data = json.load(data); data.close(); print json_data['attributes']['network']['reserved']['$i'.lower()]" | echo "export $i=`awk '{print $0}'`" >> .deployrc; done
 
 # Source .deployrc
 source .deployrc
 
 # Install necessary packages
+echo "Installing Packages.."
 apt-get install -y libvirt-bin ipmitool curl qemu-kvm
 
 # Download pxeappliance image
+echo "Downloading pxeappliance from cloud files.."
 mkdir -p /opt/rcb
 mkdir -p /mnt/pxeapp
-echo "Downloading pxeappliance from cloud files.."
 curl -o /opt/rcb/pxeappliance-dist.qcow2 ${PXE_IMAGE_URL}
 curl -o /opt/rcb/pxeappliance.xml ${PXE_XML_URL}
 
@@ -81,6 +83,7 @@ echo "Setting pxeappliance nameserver.."
 echo "nameserver 64.39.2.170" >> /mnt/pxeapp/etc/resolv.conf
 
 # Create post_install.sh and move to apache dir for later..
+echo "Creating post_install script for admin/infra node.."
 cat >post_install.sh << EOF
 #!/bin/bash
 mkdir /home/rcb/.ssh
@@ -125,6 +128,7 @@ echo 'nameserver ${NAMESERVER}' > /etc/resolv.conf
 EOF
 
 # Crowbar Appliance firstboot 
+echo "Generating firstboot script for crowbar head node.."
 cat >firstboot.sh << EOF
 #!/bin/bash
 mkdir /home/crowbar/.ssh
@@ -140,6 +144,7 @@ chmod -R a+r /etc/gemrc
 sed -i '/\/tftpboot\/ubuntu_dvd\/extra\/firstboot.sh/d' /etc/rc.local
 EOF
 
+echo "Putting scripts in place.."
 cp post_install.sh /mnt/pxeapp/var/www/post_install.sh
 cp network.json /mnt/pxeapp/var/www/network.json
 cp firstboot.sh /mnt/pxeapp/var/www/firstboot.sh
@@ -202,6 +207,7 @@ umount /mnt/pxeapp
 qemu-nbd -d /dev/nbd0
 
 # Restart libvirt-bin to avoid hvm errors..
+echo "Restart libvirt-bin.."
 /etc/init.d/libvirt-bin restart
 
 # Register domain/ boot pxeapp
@@ -210,6 +216,7 @@ virsh define /opt/rcb/pxeappliance.xml
 virsh start pxeappliance
 
 # IPMI infra node
+echo "PXE boot admin/infra node.."
 source .creds
 /usr/bin/ipmitool -H ${INFRA_DRAC} -U $DUSERNAME -P $DPASSWORD chassis bootdev pxe
 POWERSTATE=`ipmitool -H ${INFRA_DRAC} -U $DUSERNAME -P $DPASSWORD chassis status | grep System | awk '{print $4}'`
@@ -226,6 +233,7 @@ sleep 10s
 
 # WAIT FOR ADMIN NODE TO HAVE SSH
 # This takes ~30 mins
+echo "Waiting for admin node to be accessible.."
 count=1
 while [ $count -lt 30 ]; do
     count=$(( count + 1 ))
@@ -234,19 +242,42 @@ while [ $count -lt 30 ]; do
         break
     fi
     if [ $count -eq 30 ]; then
-        log "Server is not network accessible"
+        log "Admin/Infra node is not network accessible"
         exit 1
     fi
 done
 
-# Transfer Crowbar install script to admin node
-# if [ -f /home/${SUDO_USER}/.ssh/known_hosts ]; then
-    # could probably just whack it... we're unknown hosting to /dev/null
-    # ssh-keygen -f /home/${SUDO_USER}/.ssh/known_hosts -R 172.31.0.9
-# fi
-
-# ssh -i /home/${SUDO_USER}/.ssh/id_rsa.pub ${SSH_OPTS} rcb@${INFRA} 'ls -al'
-
 # Destroy pxeappliance vm/domain
 virsh destroy pxeappliance
 virsh undefine pxeappliance
+
+# WAIT FOR CROWBAR NODE TO BE RESPONSIVE
+# Giving ~30 minutes
+echo "Waiting for crowbar installation.."
+count=1
+while [ $count -lt 30]; do
+    count=$((count +1))
+    sleep 60s
+    if ( nc ${CROWBAR} 3000 -w 1 -q 0 < /dev/null ); then
+        break
+    fi
+    if [ $count -eq 30]; then
+        log "Crowbar vm did not come up."
+        exit 1
+    fi
+done
+
+# Since all nodes should be sitting in PXE we will wait a maximum of 30 minutes for all nodes to register
+echo "Waiting for all crowbar managed nodes to register.."
+count=1
+while [ $count -lt 30]; do 
+    count=$((count +1))
+    sleep 60s
+    if ( 'ssh -lcrowbar 172.31.0.10 "/opt/dell/bin/crowbar_crowbar -U crowbar -P crowbar elements | wc -l"' == ${NODECOUNT} ); then
+        break
+    fi
+    if [ $count -eq 30]; then
+        log "Some crowbar nodes did not come up."
+        exit 1
+    fi
+done 
